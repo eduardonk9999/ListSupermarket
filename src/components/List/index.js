e458e4bd9788b828d16bc34154454429ef706fa8
eduardonk9999 <thiago.novak7@gmail.com>
2021-09-29 10:07:44 -0300

    Add, function add item

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,12 +1,25 @@
 import './List.scss'
+const itens = [];
+window.handleClick = () => {
+  const itenInput = document.querySelector('.content__input').value
 
+  
+  console.log(itenInput)
+  console.log("Foi!!!")
+}
 function List() {
   return `
     <ul>
       <li>Beean</li>
       <li>Walter</li>
     </ul>
+    
   
+    <input type="text" placeholder="add item" class="content__input">
+    <button onClick="handleClick()" class="btnAdd">Add Tarefa</button>
   `
 }
+
+
+
 export default List
